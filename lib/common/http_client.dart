1cf54b54af673fabf6a2cbcc057a59a5a22e9d82
mosafarazaraaliporian <mosafarazaraaliporian@users.noreply.github.com>
2026-02-09 05:14:04 +0000

    Connect app to Cloudflare Worker API

--- lib/common/http_client.dart
+++ lib/common/http_client.dart
@@ -2,9 +2,7 @@ import 'package:dio/dio.dart';
 
 final httpClient = Dio(
   BaseOptions(
-    // TODO: Replace with your Cloudflare Worker URL after deployment
-    // Example: 'https://begzar-vpn-api.your-subdomain.workers.dev'
-    baseUrl: 'https://your-worker.workers.dev',
+    baseUrl: 'https://tight-term-75a4.mpouanfar.workers.dev',
     headers: {
       'X-Content-Type-Options': 'nosniff',
     },
